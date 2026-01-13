c0aa1bbe70e2317731063605f814b19f023ba92f
Naseer <naseern.rashid@gmail.com>
2026-01-13 10:41:18 -0500

    Restore Prisma adapter-pg for engineType=client (Heroku-safe Pool)

--- src/install-kit/prisma.js
+++ src/install-kit/prisma.js
@@ -1,6 +1,19 @@
 // src/install-kit/prisma.js
 const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { Pool } = require("pg");
 
-const prisma = new PrismaClient({});
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // important on Heroku
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 module.exports = { prisma };
